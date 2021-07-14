f1a74d4e6eb57f5f44bf6f819c9a15b76ef9d17a
Chen Asraf <chenasrafil@gmail.com>
2021-07-14 21:03:34 +0300

    major refactor

--- dist/scaffold.d.ts
+++ dist/scaffold.d.ts
@@ -1,15 +1,13 @@
-import { IScaffold } from "./index.d";
-declare class SimpleScaffold {
-    config: IScaffold.Config;
-    locals: IScaffold.Config["locals"];
-    constructor(config: IScaffold.Config);
-    private parseLocals;
-    private fileList;
-    private getFileContents;
-    private getOutputPath;
-    private writeFile;
-    private shouldWriteFile;
-    run(): void;
-    private writeDirectory;
+declare type FileResponseFn<T> = (fullPath: string, basedir: string, basename: string) => T;
+export declare type FileResponse<T> = T | FileResponseFn<T>;
+export interface ScaffoldConfig {
+    name: string;
+    templates: string[];
+    outputPath: FileResponse<string>;
+    createSubfolder?: boolean;
+    data?: Record<string, string>;
+    overwrite?: FileResponse<boolean>;
+    silent?: boolean;
 }
-export default SimpleScaffold;
+export declare function Scaffold(config: ScaffoldConfig): Promise<void>;
+export default Scaffold;
